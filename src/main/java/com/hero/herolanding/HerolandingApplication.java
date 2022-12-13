5daede364e6b655326b4e1eb8f075782d38942f4
kong-al <qaws1010@naver.com>
2022-12-13 16:42:02 +0900

    2022-12-13

---
+++ src/main/java/com/hero/herolanding/HerolandingApplication.java
@@ -0,0 +1,13 @@
+package com.hero.herolanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerolandingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerolandingApplication.class, args);
+	}
+
+}
