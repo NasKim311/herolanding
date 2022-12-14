e37f3bf67986443bc7dd45e993827c6f1ddeb933
Nas <kimna0311@naver.com>
2022-12-15 00:00:01 +0900

    domain column name work by Nas

--- src/main/java/com/hero/herolanding/domain/Board.java
+++ src/main/java/com/hero/herolanding/domain/Board.java
@@ -2,6 +2,7 @@ package com.hero.herolanding.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,30 +13,44 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@SequenceGenerator(name="boardIdSequence" , sequenceName = "member_seq" , allocationSize = 1)
-@Getter @Setter
+@SequenceGenerator(name = "boardIdSequence", sequenceName = "member_seq", allocationSize = 1)
+@Getter
+@Setter
 public class Board {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "boardIdSequence")
-	private Long 게시글번호;
-	private LocalDateTime 작성일자;
-	private LocalDateTime 수정일자;
-	private String 글제목;
-	private String 글내용;
-	private Long 조회수;
-	private Long 신고횟수;
-	private String 지역;
-	private String 게시글분류;
-	private String 닉네임;
-	private Long 회원번호;
-	private int 국가번호;
-	
-}
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "boardIdSequence")
+	@Column(name = "게시글번호")
+	private Long boardNum;
 
+	@Column(name = "작성일자")
+	private LocalDateTime insertDate;
 
+	@Column(name = "수정일자")
+	private LocalDateTime updateDate;
 
+	@Column(name = "글제목")
+	private String boardTitle;
 
+	@Column(name = "글내용")
+	private String boardContents;
 
+	@Column(name = "조회수")
+	private Long boardCount;
 
+	@Column(name = "신고횟수")
+	private Long reportCount;
 
+	@Column(name = "대륙명")
+	private String continent; // 대륙이름(ENUM)
+
+	@Column(name = "게시글분류")
+	private String boardType; // 자유게시판, 맛집후기, 여행지후기
+
+	@Column(name = "회원번호")
+	private Long memberNum; // 멤버 조인 컬럼
+
+	@Column(name = "국가번호")
+	private int countryNum; // 국가 조인 컬럼
+
+} // Board class
--- src/main/java/com/hero/herolanding/domain/ContryPaper.java
+++ src/main/java/com/hero/herolanding/domain/ContryPaper.java
@@ -1,5 +1,6 @@
 package com.hero.herolanding.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,18 +11,32 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@SequenceGenerator(name="countryPapersIdSequence" , sequenceName = "member_seq" , allocationSize = 1)
-@Getter @Setter
-public class ContryPapers {
+@SequenceGenerator(name = "countryPapersIdSequence", sequenceName = "member_seq", allocationSize = 1)
+@Getter
+@Setter
+public class ContryPaper {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "countryPapersIdSequence")
-	private int 국가별서류번호;
-	private int 나이;
-	private String 서류유효기간;
-	private String 서류제출방법;
-	private String 비고;
-	private int 국가번호;
-	private int 서류번호;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "countryPapersIdSequence")
+	@Column(name = "국가별서류번호")
+	private int countryPaperNum;
 	
-}
+	@Column(name = "최소나이")
+	private int countryPaperMinAge;
+	
+	@Column(name = "서류유효기간")
+	private String countryPaperExpiration;
+	
+	@Column(name = "서류제출방법")
+	private String countryPaperSubmitType;
+	
+	@Column(name = "비고")
+	private String countryPaperNote;
+	
+	@Column(name = "국가번호")
+	private int countryNum; // 국가 조인 컬럼
+	
+	@Column(name = "서류번호")
+	private int paperNum; // 서류 조인 컬럼
+
+} // ContryPapers class
