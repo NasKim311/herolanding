d1c6269584f7e657767d587c9ec6269ec0d66322
diddbsckd <diddbsckd456@naver.com>
2022-12-14 20:36:34 +0900

    도메인 클래스파일(report) 수정

--- src/main/java/com/hero/herolanding/domain/Report.java
+++ src/main/java/com/hero/herolanding/domain/Report.java
@@ -1,16 +1,22 @@
 package com.hero.herolanding.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@SequenceGenerator(name="reportSequence" , sequenceName = "member_seq" , allocationSize = 1)
 @Getter @Setter
 public class Report {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "reportSequence")
+	private Long 신고번호;
 	private String 신고사유;
 	private Long 신고자회원번호;
 	private Long 게시글번호;
