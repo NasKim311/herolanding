0917a8bed7405f594d958eb8f9761ef87f95c3db
kong-al <qaws1010@naver.com>
2022-12-26 16:50:37 +0900

    작업중 커밋

---
+++ src/main/java/com/hero/herolanding/controller/MailController.java
@@ -0,0 +1,13 @@
+package com.hero.herolanding.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MailController {
+	
+	
+
+}
