249c89e5c43adfdee91c74edb38a25f829665b0d
kong-al <qaws1010@naver.com>
2022-12-26 09:35:58 +0900

    검색 페이지 컬럼 수정

--- src/main/java/com/hero/herolanding/domain/EntranceLevel.java
+++ src/main/java/com/hero/herolanding/domain/EntranceLevel.java
@@ -3,13 +3,13 @@ package com.hero.herolanding.domain;
 public enum EntranceLevel {
 	EL1 ("입국금지"), EL2("백신 접종 조건부 허용") ,EL3("시설격리조치"), EL4("검역강화 및 권고사항"), EL5("한국발 입국자에 대한 입국관련 조치 해제") ;
 
-	private final String EntranceLevel;
+	private final String description;
 
-	private EntranceLevel(String EntranceLevel) {
-		this.EntranceLevel = EntranceLevel;
+	private EntranceLevel(String description) {
+		this.description = description;
 	}
 
-	public String getEntranceLevel() {
-		return EntranceLevel;
+	public String getDescription() {
+		return description;
 	}
 } // EntranceLevel enum
