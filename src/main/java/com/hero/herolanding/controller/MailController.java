7de889a3be4cd7e7131da4e5ba5d049432ac83aa
kong-al <qaws1010@naver.com>
2022-12-26 22:50:47 +0900

    수정

--- src/main/java/com/hero/herolanding/controller/MailController.java
+++ src/main/java/com/hero/herolanding/controller/MailController.java
@@ -1,13 +1,37 @@
-package com.hero.herolanding.controller;
-
-import org.springframework.stereotype.Controller;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
-public class MailController {
-	
-	
-
-}
+//package com.hero.herolanding.controller;
+//
+//import java.io.UnsupportedEncodingException;
+//
+//import javax.mail.MessagingException;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//import com.hero.herolanding.dto.MailDTO;
+//import com.hero.herolanding.service.Mailservice;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class MailController {
+//
+//	private final Mailservice mailService;
+//    
+//	
+//	@GetMapping("/mail")
+//	public String mail(Model model) {
+//		model.addAttribute("mail" , new MailDTO());
+//		return "mail";
+//	}
+//	
+//    @PostMapping("mail")
+//    public String mailConfirm(@RequestBody MailDTO emailDto) throws MessagingException, UnsupportedEncodingException {
+//        String authCode = mailService.sendEmail(emailDto.getMemberEmail());
+//        return authCode;
+//    }
+//
+//}
---
+++ src/main/java/com/hero/herolanding/dto/MailDTO.java
@@ -0,0 +1,14 @@
+package com.hero.herolanding.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MailDTO {
+
+	private String MemberEmail;
+	private String title;
+	private String contents;
+	
+	
+}
